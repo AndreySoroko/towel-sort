360ea8ed443ce5962d52d18f3169bcc768b63913
Andrey Soroko <andreysoroko84@gmail.com>
2023-02-09 13:21:32 +0300

    start task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 
-// You should implement your task here.
+function changeArrayOrder(arr) {
+  let returnArray =[];
+  for(let i = 0; i < arr.length; i++) {
+      returnArray[i] = arr[arr.length - 1 - i];
+  }
+  return returnArray;
+}
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+  let order = true; // true - direct order, false - indirect order
+  let retMatrix =[];
+  for(let i = 0; i < matrix.length; i++) {
+      if (!order) { // if inderect order - start to sort inner array
+          retMatrix[i] = changeArrayOrder(matrix[i]);
+          order = true;
+      } else {
+          
+          retMatrix[i] = matrix[i];
+          order = false;
+      }
+  }
+  let returnMatrix = [];
+      for(let i = 0; i < retMatrix.length; i++) {
+          for(let j =0; j < retMatrix[i].length; j++){
+          returnMatrix.push(retMatrix[i][j]);
+          }
+      }
+  return returnMatrix;
 }
